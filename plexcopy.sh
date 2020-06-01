2d018262fa481299335a227f86f9f7c7d38b6325
Mike Walters <mike@beringbullet.com>
2020-06-01 16:34:45 +0100

    added plex copy for cron job

--- plexcopy.sh
+++ plexcopy.sh
@@ -1,1 +1,4 @@
-sudo cp -vru /home/pi/home-pie-2/usb-drive/'TV Shows' /home/pi/media/usb-drive/Plex/tvshows/
+cd /home/pi/media/usb-drive/Plex/tvshows/
+sudo cp -vru /home/pi/home-pie-2/usb-drive/'TV Shows'/* .
+cd /home/pi/media/usb-drive/Plex/movies/
+sudo cp -vrun /home/pi/home-pie-2/usb-drive/Movies/* .
