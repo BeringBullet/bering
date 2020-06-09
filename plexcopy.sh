97af380f06799a97d06c26d7ada24f63ac3dfdbc
Mike Walters <mike@beringbullet.com>
2020-06-09 20:57:42 +0100

    update

--- plexcopy.sh
+++ plexcopy.sh
@@ -1,4 +1,2 @@
-cd /home/pi/media/usb-drive/Plex/tvshows/
-sudo mv -vfu *.mkv  /home/pi/home-pie-2/usb-drive/'TV Shows'/* .
-cd /home/pi/media/usb-drive/Plex/movies/
-sudo cp mv -vfu *.mkv  /home/pi/home-pie-2/usb-drive/Movies/* .
+rsync -vur --progress  --exclude=.* /home/pi/home-pie-2/usb-drive/'TV Shows'/* /home/pi/media/usb-drive/Plex/tvshows/
+rsync -vur --progress  --exclude=.* /home/pi/home-pi-4.8/usb/Plex/tvshows/* /home/pi/media/usb-drive/Plex/tvshows/
